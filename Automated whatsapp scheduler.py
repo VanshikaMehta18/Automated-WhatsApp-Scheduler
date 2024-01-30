e1d939e6dc208ac28f10423ab1e5ad957af3a7f2
VanshikaMehta18 <149102448+VanshikaMehta18@users.noreply.github.com>
2024-01-30 10:45:42 -0500

    Add files via upload

---
+++ Automated whatsapp scheduler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# link your whatsapp web on the browser before using this
+import pywhatkit as pwk
+import datetime
+e=datetime.datetime.now()
+
+pwk.sendwhatmsg('enter phone number here',"Hey! How have you been?",e.hour,e.minute +1) #sends a message to the number, given message, time in 24 hrs, mins 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
