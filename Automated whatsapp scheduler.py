4308baa0eb841fe0dead7f6e60e58dbec5a55ea5
VanshikaMehta18 <149102448+VanshikaMehta18@users.noreply.github.com>
2024-01-30 10:33:50 -0500

    Update Automated whatsapp scheduler.py

--- Automated whatsapp scheduler.py
+++ Automated whatsapp scheduler.py
@@ -12,27 +12,6 @@ e=datetime.datetime.now()
 pwk.sendwhatmsg('enter phone number here',"Hey! How have you been?",e.hour,e.minute +1) #sends a message to the number, given message, time in 24 hrs, mins 
 
 
-# In[8]:
-
-
-from selenium import webdriver
-
-class infow():
-    def __init__(self):
-        # Use double backslashes or a raw string for the path
-        self.driver = webdriver.Chrome(executable_path=r'D:\chromedriver_win32.exe')
-     
-    def get_info(self, query):
-        self.query = query
-        self.driver.get(url="https://www.wikipedia.org")
-
-# Create an instance of the class
-assist = infow()
-
-# Call the get_info method
-assist.get_info("hello")
-
-
 # In[ ]:
 
 
